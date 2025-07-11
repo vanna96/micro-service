4f0409e4593aac9c7017740f3be935ed7f16f426
vanna96 <sovannapoung@gmail.com>
2025-07-11 22:12:50 +0700

    update route

--- frontend-ui5/webapp/routes/master.js
+++ frontend-ui5/webapp/routes/master.js
@@ -1,45 +1,45 @@
 sap.ui.define([], function () {
-    return {
-        "routes": [
-            // category
-            {
-                "pattern": "category",
-                "name": "category",
-                "target": "category"
-            },
-            // item
-            {
-                "pattern": "item",
-                "name": "item",
-                "target": "item"
-            },
-            {
-                "pattern": "item/create",
-                "name": "item_create",
-                "target": "item_form"
-            }
-        ],
-        "targets": {
-            // category
-            "category": {
-                "viewName": "master_data.category.list",
-                "controlId": "appContainer",
-                "parent": "main",
-                "viewLevel": 1
-            },
-            // item
-            "item": {
-                "viewName": "master_data.item.list",
-                "controlId": "appContainer",
-                "parent": "main",
-                "viewLevel": 1
-            },
-            "item_form": {
-                "viewName": "master_data.item.form",
-                "controlId": "appContainer",
-                "parent": "main",
-                "viewLevel": 1
-            }
+    const routes = [
+        {
+            "pattern": "category",
+            "name": "category",
+            "target": "category"
+        },
+        {
+            "pattern": "item",
+            "name": "item",
+            "target": "item"
+        },
+        {
+            "pattern": "item/create",
+            "name": "item_create",
+            "target": "item_form"
         }
+    ];
+
+    const targets = {
+        "category": {
+            "viewName": "master_data.category.list",
+            "controlId": "appContainer",
+            "parent": "main",
+            "viewLevel": 1
+        },
+        "item": {
+            "viewName": "master_data.item.list",
+            "controlId": "appContainer",
+            "parent": "main",
+            "viewLevel": 1
+        },
+        "item_form": {
+            "viewName": "master_data.item.form",
+            "controlId": "appContainer",
+            "parent": "main",
+            "viewLevel": 1
+        }
+    }
+
+    return {
+        routes,
+        targets
     };
 });
